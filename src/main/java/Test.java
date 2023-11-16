db1527cbcec7ef1b12fafe1063ceb50d277979b9
JZWen <ziwen_jiang@qq.com>
2023-11-16 22:02:33 +0800

    Update : init project

---
+++ src/main/java/Test.java
@@ -0,0 +1,6 @@
+/**
+ * @author: JZWen
+ * @date:2023/11/16 10:01 下午
+ */
+public class Test {
+}
---
+++ src/main/java/interview150/Demo01.java
@@ -0,0 +1,8 @@
+package interview150;
+
+/**
+ * @author: JZWen
+ * @date:2023/11/16 10:02 下午
+ */
+public class Demo01 {
+}
